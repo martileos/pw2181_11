2285b7b8a89d9e5935cf73636bc6505db63eefde
martileos <martinevarez@gmail.com>
2018-05-21 11:55:30 -0600

    Inicio de proyecto

---
+++ servidor/primera.php
@@ -0,0 +1,19 @@
+<?php
+    $parametro1 = $_GET["parametro1"];
+	$variable = 0;
+	$otra = "";
+	$otradeotra = 0.0;
+	
+	for ($i=0; $i <= 10 ; $i++) 
+	{ 
+		print("Texto ".$i." <br>");
+	}
+?>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	Hola mundo <?php print($parametro1);  ?>
+</body>
+</html>
