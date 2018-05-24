cfbf394b47d695cced6764dbf64ce47a2eec8b0c
martileos <martinevarez@gmail.com>
2018-05-24 12:08:48 -0600

    Buscar usuarios

--- servidor/js/eventos.js
+++ servidor/js/eventos.js
@@ -17,6 +17,10 @@ var inicioApp = function(){
 				if(response.respuesta == true){
 					//alert("Bienvenido");
 					$("#secInicio").hide("slow");
+					$("#frmUsuarios").show("slow");
+					//Posiciona el cursor en el cuadro de
+					//texto.
+					$("#txtNombreUsuario").focus();
 				}else{
 					alert("usuario o clave incorrecta(s)");
 				}
@@ -26,7 +30,40 @@ var inicioApp = function(){
 			}
 		});
 	}
+	var buscaUsuario = function(){
+		var usuario = $("#txtNombreUsuario").val();
+		var parametros="opc=buscaUsuario"+
+					   "&usuario="+usuario+
+					   "&aleatorio="+Math.random();					   	
+		if(usuario != ""){
+			$.ajax({
+				cache:false,
+				type: "POST",
+				dataType: "json",
+				url: "php/buscausuario.php",
+				data: parametros,
+				success:function(response){
+					if(response.respuesta == true){
+						$("#txtNombre").val(response.nombre);
+						$("#txtClaveUsuario").val(response.clave);
+					}else{
+						$("#txtNombre").focus();
+					}	
+				},
+				error:function(xhr,ajaxOptions,thrownError){
+
+				}
+			});	
+		}
+	}
+	var teclaNombreUsuario = function(tecla){
+		if(tecla.which == 13){ //Enter
+			buscaUsuario();
+		}
+	}
 	$("#btnAceptar").on("click",Aceptar);
+	$("#txtNombreUsuario").on("keypress",teclaNombreUsuario);
+	$("#frmUsuarios").hide();
 }
 $(document).ready(inicioApp);
 
---
+++ servidor/php/buscausuario.php
@@ -0,0 +1,43 @@
+<?php
+include 'conexiones.php';
+function buscausuario(){
+	$respuesta = false;
+	$usuario = $_POST["usuario"];
+	//Conectarnos al servidor de BD.
+	$con=conecta();
+	$consulta="select * from usuarios where usuario= '".$usuario."' limit 1";
+	$resConsulta=mysqli_query($con,$consulta);
+	$nombre = "";
+	$clave  = "";
+	if(mysqli_num_rows($resConsulta) > 0){
+		$respuesta = true;
+		while($regConsulta = mysqli_fetch_array($resConsulta)){
+			$nombre = $regConsulta["nombre"];
+			$clave  = $regConsulta["clave"];
+		}
+
+	}
+	$salidaJSON = array('respuesta' => $respuesta,
+						'nombre'    => $nombre,
+						'clave'     => $clave );
+	print json_encode($salidaJSON);
+}
+
+$opc = $_POST["opc"];
+switch ($opc) {
+	case 'buscaUsuario':
+		buscausuario();
+		break;
+	
+	default:
+		# code...
+		break;
+}
+?>
+
+
+
+
+
+
+
